6d1c2e730e38de1d102434eddd75ae027ed70e48
carju11 <124043777+carju11@users.noreply.github.com>
2025-05-19 18:02:47 +0200

    Fallo corregido

--- src/sistema/eventos/interfaces/IComprarEntrada.java
+++ src/sistema/eventos/interfaces/IComprarEntrada.java
@@ -2,7 +2,7 @@ package sistema.eventos.interfaces;
 
 public interface IComprarEntrada {
     
-    boolean comprobarEntradas(nEntradasAdquirir:List<Entrada>); //confirma las entradas que el usuario va a comprar
+boolean comprobarEntradas(List<Entrada> nEntradasAdquirir); // confirma las entradas que el usuario va a comprar
     boolean pagoDeEntradas(int idUsuario, String FormaPago); //realiza el pago de las entradas
     Entrada generarEntradaUsuario(Entrada entradaAdquirida, int idUsuario); //se genera la entrada para el usuario y se le asocia
 
